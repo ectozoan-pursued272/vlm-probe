f017ce27dd3d62a534a7c9b73b6e5357e1b20e25
Xu Mingrui <255718431+marived@users.noreply.github.com>
2023-12-23 23:20:28 +0800

    tasks: scorer that strips 'there are', extracts first number

--- vlmprobe/tasks.py
+++ vlmprobe/tasks.py
@@ -11,3 +11,15 @@ def load_task(path):
 
 def score_exact(pred, ref):
     return pred.strip().lower() == ref.strip().lower()
+
+
+import re
+
+
+def parse_number(text):
+    m = re.search(r"-?\d+", text)
+    return m.group(0) if m else text.strip()
+
+
+def score_count(pred, ref):
+    return parse_number(pred) == ref.strip()
